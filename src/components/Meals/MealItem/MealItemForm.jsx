830e6e64966fad2c7ce6c9b608b3bfe23d7141eb
snare-dev <amanisanare1@gmail.com>
2022-12-29 20:58:40 -0500

    fixed stying bugs

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -8,13 +8,13 @@ const MealItemForm = (props) => {
     <form className={classes.form}>
       <Input
         label="Amount"
-        Input={{
-          id: "amount",
+        input={{
+          id: "amount_"+props.id,
           type: "number",
           min: "1",
           max: "5",
           step: "1",
-          defaultValue: "1",
+          defaultValue: "1"
         }}
       />
       <button>+ Add</button>
